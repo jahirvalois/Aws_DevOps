f1fb1f4f9afd2dc40881f4df372093f1b9d04660
Jahir Valois <jahirvalois@outlook.com>
2022-04-29 19:05:28 -0400

    Deplyment IGW

--- Terraform/4-20-2022/deployments/vpc1/main.tf
+++ Terraform/4-20-2022/deployments/vpc1/main.tf
@@ -1,6 +1,29 @@
 module "vpc" {
   source = "../../Modules/vpc"
-  cidr = locals.cidr
-  region = locals.region
-  vpc_name = locals.vpc_name
+  cidr = local.cidr
+  region = local.region
+  vpc_name = local.vpc_name
+}
+
+module "private_subnet" {
+  source = "../../Modules/subnet"
+  region = local.region
+  vpc_name = local.vpc_name
+  subnet_cidr = local.private_subnet_cidr
+  subnet_name = local.private_subnet_name
+}
+
+module "public_subnet" {
+  source = "../../Modules/subnet"
+  region = local.region
+  vpc_name = local.vpc_name
+  subnet_cidr = local.public_subnet_cidr
+  subnet_name = local.public_subnet_name
+}
+
+module "igw" {
+  source = "../../Modules/igw"
+  region = local.region
+  vpc_name = local.vpc_name
+  igw_name = local.igw_name
 }
